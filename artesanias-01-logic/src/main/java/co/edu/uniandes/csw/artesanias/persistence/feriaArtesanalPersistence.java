a37036e07307ab40c1ad6aa0b0edf3f015ace2c0
jmmunoz14 <jm.munoz14@uniandes.edu.co>
2017-03-13 18:49:02 -0500

    Se agrego la clase Persistance junto con gran cantidad de sus metodos, destacando crear, update, editar, obtener y obtener una lista para: feria, pabellon, stand y usuario.

--- artesanias-01-logic/src/main/java/co/edu/uniandes/csw/artesanias/persistence/feriaArtesanalPersistence.java
+++ artesanias-01-logic/src/main/java/co/edu/uniandes/csw/artesanias/persistence/feriaArtesanalPersistence.java
@@ -5,7 +5,15 @@
  */
 package co.edu.uniandes.csw.artesanias.persistence;
 
+import co.edu.uniandes.csw.artesanias.entities.FeriaArtesanalEntity;
+import co.edu.uniandes.csw.artesanias.entities.PabellonEntity;
+import co.edu.uniandes.csw.artesanias.entities.StandEntity;
+import co.edu.uniandes.csw.artesanias.entities.UsuarioEntity;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -13,5 +21,134 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class feriaArtesanalPersistence {
+    @PersistenceContext(unitName="feriaPU")
+    protected EntityManager em;
+    
+    
+        public FeriaArtesanalEntity createFeria (FeriaArtesanalEntity entity) 
+        {
+      
+        em.persist(entity);
+        
+        return entity;
+    
+        }
+        
+        public FeriaArtesanalEntity findFeria (Long id) 
+        {
+      
+        return em.find(FeriaArtesanalEntity.class, id);
+   
+        }
+        public FeriaArtesanalEntity updateFeria(FeriaArtesanalEntity entity) 
+        {
+       
+        return em.merge(entity);
+   
+        }
+        
+        public void deleteFeria (Long id) {
+        
+        FeriaArtesanalEntity entity = em.find(FeriaArtesanalEntity.class, id);
+        em.remove(entity);
+    }
+        public PabellonEntity createPabellon (PabellonEntity entity)
+        {
+            em.persist(entity);
+            
+            return entity;
+        }
+        
+         public PabellonEntity findPabellon (Long id) 
+        {
+      
+        return em.find(PabellonEntity.class, id);
+   
+        }
+        public PabellonEntity updatePabellon(PabellonEntity entity) 
+        {
+       
+        return em.merge(entity);
+   
+        } 
+        
+        public void deletePabellon (Long id) {
+        
+        PabellonEntity entity = em.find(PabellonEntity.class, id);
+        em.remove(entity);
+    }
+         
+        public List<PabellonEntity> findAllPabellones()
+    {
+        TypedQuery <PabellonEntity> q = em.createQuery("select u from PabellonEntity u" , PabellonEntity.class);
+        return q.getResultList();
+        
+    }
+        
+    public StandEntity createStand (StandEntity entity)
+        {
+            em.persist(entity);
+            
+            return entity;
+        }
+        
+         public StandEntity findStand (Long id) 
+        {
+      
+        return em.find(StandEntity.class, id);
+   
+        }
+        public StandEntity updateStand(StandEntity entity) 
+        {
+       
+        return em.merge(entity);
+   
+        } 
+        
+        public void deleteStand (Long id) {
+        
+        StandEntity entity = em.find(StandEntity.class, id);
+        em.remove(entity);
+    }
+         
+        public List<StandEntity> findAllStands()
+    {
+        TypedQuery <StandEntity> q = em.createQuery("select u from PabellonEntity u" , StandEntity.class);
+        return q.getResultList();
+        
+    }
+        
+            public UsuarioEntity createUsuario (UsuarioEntity entity)
+        {
+            em.persist(entity);
+            
+            return entity;
+        }
+        
+         public UsuarioEntity findUsuario (Long id) 
+        {
+      
+        return em.find(UsuarioEntity.class, id);
+   
+        }
+        public UsuarioEntity updateUsuario(UsuarioEntity entity) 
+        {
+       
+        return em.merge(entity);
+   
+        } 
+        
+        public void deleteUsuario (Long id) {
+        
+        UsuarioEntity entity = em.find(UsuarioEntity.class, id);
+        em.remove(entity);
+    }
+         
+        public List<UsuarioEntity> findAllUsuarios()
+    {
+        TypedQuery <UsuarioEntity> q = em.createQuery("select u from PabellonEntity u" , UsuarioEntity.class);
+        return q.getResultList();
+        
+    }
     
 }
