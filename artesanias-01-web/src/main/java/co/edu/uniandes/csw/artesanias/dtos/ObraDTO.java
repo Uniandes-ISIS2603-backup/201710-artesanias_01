c9f8e6415c795791effb08e13f6750017e0f2a36
James Lake <jh.lake@uniandes.edu.co>
2017-03-15 00:18:35 -0500

    DTOS - James

---
+++ artesanias-01-web/src/main/java/co/edu/uniandes/csw/artesanias/dtos/ObraDTO.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.artesanias.dtos;
+
+/**
+ *
+ * @author jlake
+ */
+import co.edu.uniandes.csw.artesanias.entities.ObraEntity;
+import co.edu.uniandes.csw.artesanias.entities.StandEntity;
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class ObraDTO implements Serializable{
+    
+    private int id;
+    private String nombre;
+    private int rating;
+    private String materiales;
+    private String tecnica;
+    
+    public ObraDTO(){}
+    
+    public ObraDTO(ObraEntity entity)
+    {
+        if(entity !=null)
+        {
+            this.id = entity.getId();
+            this.nombre = entity.getNombre();
+            this.rating = entity.getRating();
+            this.materiales = entity.getMaterial();
+            this.tecnica = entity.getTecnica();
+        }
+    }
+    
+    public ObraEntity toEntity()
+    {
+        ObraEntity entity = new ObraEntity();
+        entity.setId(id);
+        entity.setMaterial(materiales);
+        entity.setRating(rating);
+        entity.setTecnica(tecnica);
+        entity.setNombre(nombre);
+        return entity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getMateriales() {
+        return materiales;
+    }
+
+    public void setMateriales(String materiales) {
+        this.materiales = materiales;
+    }
+
+    public String getTecnica() {
+        return tecnica;
+    }
+
+    public void setTecnica(String tecnica) {
+        this.tecnica = tecnica;
+    }
+    
+    
+}
