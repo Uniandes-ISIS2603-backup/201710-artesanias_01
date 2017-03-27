ee8be0411cade94a7f131ba944f33fdd46ddbf59
jm.munoz14 <jm.munoz14@uniandes.edu.co>
2017-03-26 20:32:13 -0500

    se corrigieron los imports

--- artesanias-01-logic/src/main/java/co/edu/uniandes/csw/artesanias/persistence/PabellonPersistence.java
+++ artesanias-01-logic/src/main/java/co/edu/uniandes/csw/artesanias/persistence/PabellonPersistence.java
@@ -5,13 +5,16 @@
  */
 package co.edu.uniandes.csw.artesanias.persistence;
 
-@Stateless
+
+
 import co.edu.uniandes.csw.artesanias.entities.PabellonEntity;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+
+@Stateless
 /**
  *
  * @author jm.munoz14
