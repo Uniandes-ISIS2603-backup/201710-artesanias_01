f7768c50611711ef10dd273841455ce651325a26
f.velasquez11 <f.velasquez11@ISIS2603-0178.sis.virtual.uniandes.edu.co>
2017-04-04 15:34:15 -0500

    Se implementa bower, angulas, bootstrap, y se hace el index de la aplicaciòn.

--- artesanias-01-web/src/main/webapp/src/app.js
+++ artesanias-01-web/src/main/webapp/src/app.js
@@ -22,55 +22,25 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 (function (ng) {
-    var mod = ng.module('mainApp', [
-        //'ngCrudMock',
-        'ngCrud',
+    var app = angular.module('mainApp', [
+        // External dependencies
         'ui.router',
-        'ui.grid',
-        'employeeModule',
-        'authModule'
+        // Internal modules dependencies       
+        'anfitrionModule',
+        'viajeroModule',
+        'resenaModule',
+        'habitacionModule',
+        'disponibilidadModule',
+        'reservaModule',
+        'viviendaModule',
+        'pagoModule'
+
+
+        
     ]);
-
-    mod.constant('baseUrl', 'api');
-
-    mod.config(['$logProvider', function ($logProvider) {
-            $logProvider.debugEnabled(true);
-        }]);
-
-    mod.config(['RestangularProvider', 'baseUrl', function (rp, baseUrl) {
-            rp.setBaseUrl(baseUrl);
-            rp.setRequestInterceptor(function (elem, operation) {
-                if (operation === "remove") {
-                    return null;
-                }
-                return elem;
-            });
-            rp.addResponseInterceptor(function (data, operation, what, url, response) {
-                if (operation === "getList") {
-                    data.totalRecords = parseInt(response.headers("X-Total-Count")) || 1;
-                }
-                return data;
-            });
-        }]);
-
-    mod.config(['$urlRouterProvider', function ($urlRouterProvider) {
-                $urlRouterProvider.otherwise('/');
-        }]);
-
-    mod.config(['authServiceProvider', 'baseUrl', function (auth, baseUrl) {
-            auth.setValues({
-                apiUrl: baseUrl + '/users/',
-                successState: 'employeeList'
-            });
-        }]);
-
-    /*
-     * When there's an error changing state, ui-router doesn't raise an error
-     * This configuration allows to print said errors
-     */
-    mod.run(['$rootScope', '$log', function ($rootScope, $log) {
-            $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
-                $log.warn(error);
-            });
+    // Resuelve problemas de las promesas
+    app.config(['$qProvider', function ($qProvider) {
+            $qProvider.errorOnUnhandledRejections(false);
+             
         }]);
 })(window.angular);
