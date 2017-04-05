14b56fa5bb37c88db957b5024ef024214f251a5b
James Lake <jh.lake@uniandes.edu.co>
2017-04-05 09:17:38 -0500

    Add html fix

--- artesanias-01-web/src/main/webapp/src/modules/usuario/usuario.mod.js
+++ artesanias-01-web/src/main/webapp/src/modules/usuario/usuario.mod.js
@@ -3,5 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//autor: Felipe Velàsquez (f.velasquez11/SarkFox)
 
+(function (ng) {
+    var mod = ng.module("usuarioModule", ['ui.router']);
+    
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/Usuario/';
+            $urlRouterProvider.otherwise("/usuariosList");
+
+            $stateProvider.state('usuario', {
+                url: '/usuarios',
+                abstract: true,
+                resolve: {
+                    usuarios: ['$http', function ($http) {
+                            console.log("Holi");
+                            return $http.get('data/usuarios.json');
+                        }]
+                },
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'usuario.html',
+                        controller: ['$scope', 'usuarios', function ($scope, usuarios) {
+                                console.log("Holi2");
+                                $scope.usuariosRecords = usuarios.data;
+                            }]
+                    }
+                }
+            }).state('usuariosList', {
+                url: '/usuarios/list',
+                resolve: {
+                    usuarios: ['$http', function ($http) {
+                            console.log("3");
+                            return $http.get('data/usuarios.json');
+                        }]
+                },
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'usuarios.list.html',
+                        controller: ['$scope', 'usuarios', function ($scope, usuarios) {
+                                console.log("Holi4");
+                                $scope.usuariosRecords = usuarios.data;
+                            }]
+                    }
+                }
+            });
+        }]);
+})(window.angular);
 
