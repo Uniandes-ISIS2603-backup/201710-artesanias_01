723d37f65ce5def1f2dd27eba6cb89df6eb3f819
FELIPE VELASQUEZ JARAMILLO <f.velasquez11@sis.virtual.uniandes.edu.co>
2017-04-07 09:55:56 -0500

    Agregar archivos

--- artesanias-01-web/src/main/webapp/src/modules/usuario/usuario.mod.js
+++ artesanias-01-web/src/main/webapp/src/modules/usuario/usuario.mod.js
@@ -2,52 +2,72 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * 
+ * 
+ * Autor: Felipe Velásquez/f.velasquez11/SarkFox
  */
-//autor: Felipe Velàsquez (f.velasquez11/SarkFox)
+
 
 (function (ng) {
-    var mod = ng.module("usuarioModule", ['ui.router']);
-    
-    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            var basePath = 'src/modules/Usuario/';
-            $urlRouterProvider.otherwise("/usuariosList");
-
-            $stateProvider.state('usuario', {
-                url: '/usuarios',
-                abstract: true,
-                resolve: {
-                    usuarios: ['$http', function ($http) {
-                            console.log("Holi");
-                            return $http.get('data/usuarios.json');
-                        }]
-                },
-                views: {
-                    'mainView': {
-                        templateUrl: basePath + 'usuario.html',
-                        controller: ['$scope', 'usuarios', function ($scope, usuarios) {
-                                console.log("Holi2");
-                                $scope.usuariosRecords = usuarios.data;
-                            }]
-                    }
-                }
-            }).state('usuariosList', {
-                url: '/usuarios/list',
-                resolve: {
-                    usuarios: ['$http', function ($http) {
-                            console.log("3");
-                            return $http.get('data/usuarios.json');
-                        }]
-                },
-                views: {
-                    'mainView': {
-                        templateUrl: basePath + 'usuarios.list.html',
-                        controller: ['$scope', 'usuarios', function ($scope, usuarios) {
-                                console.log("Holi4");
-                                $scope.usuariosRecords = usuarios.data;
-                            }]
-                    }
-                }
-            });
-        }]);
-})(window.angular);
-
+     var mod = ng.module("usuarioModule", ['ui.router']);
+     
+     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+             var basePath = 'src/modules/Usuario/';
+             $urlRouterProvider.otherwise("/usuariosList");
+ 
+             $stateProvider.state('usuario', {
+                 url: '/usuarios',
+                 abstract: true,
+                 resolve: {
+                     usuarios: ['$http', function ($http) {
+                             console.log("Holi");
+                             return $http.get('data/dataUsuarios.json');
+                         }]
+                 },
+                 views: {
+                     'mainView': {
+                         templateUrl: basePath + 'usuario.html',
+                         controller: ['$scope', 'usuarios', function ($scope, usuarios) {
+                                 console.log("Holi2");
+                                 $scope.usuariosRecords = usuarios.data;
+                             }]
+                     }
+                 }
+             }).state('usuariosList', {
+                 url: '/usuarios/list',
+                 resolve: {
+                     usuarios: ['$http', function ($http) {
+                             console.log("3");
+                             return $http.get('data/dataUsuarios.json');
+                         }]
+                 },
+                 views: {
+                     'mainView': {
+                         templateUrl: basePath + 'usuario.list.html',
+                         controller: ['$scope', 'usuarios', function ($scope, usuarios) {
+                                 console.log("Holi4");
+                                 $scope.usuariosRecords = usuarios.data;
+                             }]
+                     }
+                 }
+             }).state('usuariosDetail', {
+                 url: '/{usuarioId:int}/detail',
+                 parent: 'usuario',
+                 param: {
+                     usuarioId: null
+                 },
+                 views: {
+                     'detailView': {
+                         templateUrl: basePath + 'usuario.detail.html',
+                         controller: ['$scope', '$stateParams', function ($scope, $params) {
+                         
+                                 $scope.currentUsuario = $scope.usuariosRecords[$params.usuarioId - 1];
+                             }]
+                     }
+ 
+                 }
+ 
+             });
+         }]);
+         
+ })(window.angular); 
