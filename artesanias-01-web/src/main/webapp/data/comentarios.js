ea6a24d6a75a2330240814aadeaf66695aef60ee
da.cortes11 <da.cortes11@ISIS2603-0131.sis.virtual.uniandes.edu.co>
2017-04-06 20:09:14 -0500

    cree modulos comentario y conferencia y edite app.js

---
+++ artesanias-01-web/src/main/webapp/data/comentarios.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+[{"id":1,"comentario":"sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum felis sed","fecha":"8/30/2016","obra":"obra7"},
+{"id":2,"comentario":"id nulla ultrices aliquet maecenas leo odio condimentum id luctus nec molestie sed justo","fecha":"9/1/2016","obra":"obra4"},
+{"id":3,"comentario":"eget semper rutrum nulla nunc purus phasellus in felis donec semper sapien a libero nam","fecha":"3/19/2017","obra":"obra1"},
+{"id":4,"comentario":"ut ultrices vel augue vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae donec","fecha":"9/11/2016","obra":"obra2"},
+{"id":5,"comentario":"penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum","fecha":"6/13/2016","obra":"obra1"},
+{"id":6,"comentario":"integer aliquet massa id lobortis convallis tortor risus dapibus augue vel accumsan tellus nisi eu orci mauris lacinia sapien quis","fecha":"9/20/2016","obra":"obra4"},
+{"id":7,"comentario":"odio in hac habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla","fecha":"5/31/2016","obra":"obra8"},
+{"id":8,"comentario":"ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae mauris viverra diam","fecha":"12/25/2016","obra":"obra3"},
+{"id":9,"comentario":"montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis","fecha":"12/22/2016","obra":"obra5"},
+{"id":10,"comentario":"congue vivamus metus arcu adipiscing molestie hendrerit at vulputate vitae nisl aenean lectus pellentesque eget nunc donec quis","fecha":"9/18/2016","obra":"obra7"},
+{"id":11,"comentario":"cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis","fecha":"10/11/2016","obra":"obra6"},
+{"id":12,"comentario":"erat nulla tempus vivamus in felis eu sapien cursus vestibulum proin eu mi nulla ac enim","fecha":"10/3/2016","obra":"obra6"},
+{"id":13,"comentario":"maecenas tincidunt lacus at velit vivamus vel nulla eget eros elementum pellentesque quisque","fecha":"8/14/2016","obra":"obra2"}]
