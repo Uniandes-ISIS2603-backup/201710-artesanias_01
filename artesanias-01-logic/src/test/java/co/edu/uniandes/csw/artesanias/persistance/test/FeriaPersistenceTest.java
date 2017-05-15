7a7e40b878246850d05b4c91b2a93b4607135648
ds.tapia10 <ds.tapia10@ISIS2603-0132.sis.virtual.uniandes.edu.co>
2017-05-15 12:04:33 -0500

    inicio feriaPersistenceTest

---
+++ artesanias-01-logic/src/test/java/co/edu/uniandes/csw/artesanias/persistance/test/FeriaPersistenceTest.java
@@ -0,0 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.uniandes.csw.artesanias.persistance.test;
+
+import co.edu.uniandes.csw.artesanias.ejbs.ReservadoLogic;
+import co.edu.uniandes.csw.artesanias.entities.FeriaArtesanalEntity;
+import co.edu.uniandes.csw.artesanias.persistence.feriaArtesanalPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+import java.util.logging.Level;
+//import org.jboss.arquillian.junit.Arquillian;
+//import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author ds.tapia10
+ */
+//@RunWith(Arquillian.class)
+public class FeriaPersistenceTest {
+    
+    private static final Logger logger = Logger.getLogger(ReservadoLogic.class.getName());
+
+    @Inject
+    feriaArtesanalPersistence persistence;
+
+    @PersistenceContext(unitName = "feriaPU")
+    protected EntityManager em;
+
+    @Inject
+    UserTransaction utx;
+
+    @Deployment
+    public static JavaArchive creaDespliege() {
+        logger.info("Generando archivo de despliegue");
+        JavaArchive archive = ShrinkWrap.create(JavaArchive.class)
+                .addPackage(FeriaArtesanalEntity.class.getPackage())
+                .addPackage(feriaArtesanalPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml")
+                .addAsManifestResource("META-INF/logging.properties", "logging.properties");
+        logger.info(archive.toString());
+        return archive;
+    }
+
+    @Before
+    @SuppressWarnings("UseSpecificCatch")
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } 
+        catch (Exception e) {
+            logger.log(Level.FINE, "Error creando datos de prueba", e);
+            try {
+                utx.rollback();
+            } 
+            catch (Exception e1) {
+                logger.log(Level.FINE, "Error haciendo rollback de datos de prueba", e1);
+            }
+        }
+    }
+
+    // borra las tablas
+    private void clearData() {
+      em.createQuery("delete from FeriaArtesanalEntity").executeUpdate();
+    }
+
+    // datos de prueba
+    private List<FeriaArtesanalEntity> data = new ArrayList<>();
+
+    // inserta datos de prueba en las tablas
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            FeriaArtesanalEntity entity = factory.manufacturePojo(FeriaArtesanalEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+        
+    /**
+     * Prueba para crear un Book.
+     *
+     * @generated
+     */
+    @Test
+    public void createFeriaTest() {   
+        //PodamFactory factory = new PodamFactoryImpl();
+        //FeriaArtesanalEntity newEntity = factory.manufacturePojo(FeriaArtesanalEntity.class);
+        
+        //Assert.assertNotNull(newEntity);
+       
+        //FeriaArtesanalEntity result = persistence.createFeria(newEntity);
+
+        //Assert.assertNotNull(result);
+
+        //FeriaArtesanalEntity entity = em.find(FeriaArtesanalEntity.class, result.getId());
+                   
+        //Assert.assertEquals(newEntity.getFechaFin(), entity.getFechaFin());
+        //Assert.assertEquals(newEntity.getFechaInicio(), entity.getFechaInicio());
+        //Assert.assertEquals(newEntity.getId(), entity.getId());
+        //Assert.assertEquals(newEntity.getLugar(), entity.getLugar());
+        //Assert.assertEquals(newEntity.getNombre(), entity.getNombre());
+        
+         Assert.assertTrue(true);
+    }
+    
+    /**
+     * Prueba para consultar la lista de Books.
+     *
+     * @generated
+     */
+    @Test
+    public void findFeriaTest() {
+        Assert.assertTrue(true);
+    }
+
+    /**
+     * Prueba para consultar un Book.
+     *
+     * @generated
+     */
+    @Test
+    public void findAllFeriasTest() {
+        Assert.assertTrue(true);
+    }
+    
+    /**
+     * Prueba para eliminar un Book.
+     *
+     * @generated
+     */
+    @Test
+    public void updateFeriaTest() {
+        Assert.assertTrue(true);        
+    }
+
+    /**
+     * Prueba para actualizar un Book.
+     *
+     * @generated
+     */
+    @Test
+    public void deleteFeriaTest() {
+        Assert.assertTrue(true);
+    }
+    
+}
